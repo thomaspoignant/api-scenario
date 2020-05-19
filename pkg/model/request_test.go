3f363c5f644d5b9d52e5baee15863c7211d3f315
Thomas Poignant <thomas.poignant@gmail.com>
2020-05-19 12:35:57 +0200

    Add test to override headers from the command line

--- pkg/model/request_test.go
+++ pkg/model/request_test.go
@@ -5,10 +5,15 @@ import (
 	"github.com/thomaspoignant/api-scenario/pkg/model"
 	"github.com/thomaspoignant/api-scenario/test"
 	"testing"
+
+	"github.com/spf13/viper"
 )
 
-func Test_PatchWithContext_valid(t *testing.T) {
-	req := model.Request{
+
+var req model.Request
+
+func initReq() {
+	req =  model.Request{
 		Request: &rest.Request{
 			Body:    []byte(`{"hello":"world_{{random_int(1,1)}}"}`),
 			BaseURL: "http://perdu.com/{{random_int(1,1)}}",
@@ -21,7 +26,10 @@ func Test_PatchWithContext_valid(t *testing.T) {
 			Method: rest.Get,
 		},
 	}
+}
 
+func Test_PatchWithContext_valid(t *testing.T) {
+	initReq()
 	want := []model.ResultVariable{
 		{Key: "body", NewValue: "{\"hello\":\"world_1\"}", Type: model.Used},
 		{Key: "url", NewValue: "http://perdu.com/1", Type: model.Used},
@@ -29,7 +37,33 @@ func Test_PatchWithContext_valid(t *testing.T) {
 		{Key: "headers.Content-Type", NewValue: "other_test_1", Type: model.Used},
 	}
 	got := req.PatchWithContext()
-
 	test.Equals(t, "", want, got)
+}
 
+func Test_OverrideHeadersFromViper(t *testing.T) {
+	initReq()
+	want := map[string]string{
+		"Content-Type": "application/JSON",
+		"Accept": "application/JSON",
+	}
+	viper.Set("headers", want)
+	req.AddHeadersFromFlags()
+	got := req.Headers
+
+	test.Equals(t, "Header should be override", want, got)
+}
+
+func Test_AddHeadersFromViper(t *testing.T) {
+	initReq()
+	want := map[string]string{
+		"Accept": "application/JSON",
+		"Content-Type":"other_test_{{random_int(1,1)}}",
+	}
+	viper.Set("headers", map[string]string{
+		"Accept": "application/JSON",
+	})
+	req.AddHeadersFromFlags()
+	got := req.Headers
+
+	test.Equals(t, "Header should be override", want, got)
 }
