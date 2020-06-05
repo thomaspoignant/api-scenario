0f99aa31c888c5f1b0657e852147b6868ffc4614
Thomas Poignant <thomas.poignant@gmail.com>
2020-06-05 12:54:45 +0200

    remove duplicate

--- test/mock_rest.go
+++ test/mock_rest.go
@@ -7,33 +7,34 @@ import (
 type ClientMock struct {
 }
 
+var jsonBody = `{
+				"hello":"world",
+				"param1":true,
+				"param2":123
+				}`
+
+var xmlBody =`<root>
+				<hello>world</hello>
+				<param1>true</param1>
+				<param2>123</param2>
+		   	  </root>`
+
 func (c *ClientMock) Send(request rest.Request) (*rest.Response, error) {
 	testNumber:=request.QueryParams["testNumber"]
+
+	response := &rest.Response{
+		StatusCode: 200,
+		Headers: map[string][]string{
+			"Content-Type": {"application/json"},
+		},
+	}
+
 	if testNumber == "1" {
-		return &rest.Response{
-			Body: `{
-					"hello":"world",
-					"param1":true,
-					"param2":123
-					}`,
-			StatusCode: 200,
-			Headers: map[string][]string{
-				"Content-Type": {"application/json"},
-			},
-		}, nil
+		response.Body = jsonBody
+		return response, nil
 	} else if testNumber == "2" {
-		return &rest.Response{
-			Body: `<root>
-						<hello>world</hello>
-						<param1>true</param1>
-						<param2>123</param2>
-				   </root>
-					`,
-			StatusCode: 200,
-			Headers: map[string][]string{
-				"Content-Type": {"application/json"},
-			},
-		}, nil
+		response.Body = xmlBody
+		return response, nil
 	}
 
 	return &rest.Response{}, nil
